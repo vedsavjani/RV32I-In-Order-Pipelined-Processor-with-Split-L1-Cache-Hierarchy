f7a62174101c0ef030da1e7f392bc62a93cd3c14
vedsavjani <veddsavjani@gmail.com>
2026-07-14 16:05:03 +0530

    pipeline registers

--- rtl/data_pipes.sv
+++ rtl/data_pipes.sv
@@ -1,19 +1,19 @@
 module PCReg(
-    input logic clk, enn,
+    input logic clk, enn, reset,
     input logic [31:0] pcnext,
     output logic [31:0] pcF);
 
     initial pcF = 32'h00000000;
 
     always_ff @(posedge clk) begin
-        pcF <= enn? pcF: pcnext;
+        pcF <= reset ? 32'h0 : (enn ? pcF : pcnext);
     end
 
 endmodule
 
 
 module IF_ID_datapipe(
-    input logic clk, enn, clr,
+    input logic clk, enn, clr, reset,
     input logic [31:0] instrF,
     output logic [31:0] instrD,
     input logic [31:0] pcF,
@@ -22,12 +22,12 @@ module IF_ID_datapipe(
     output logic [31:0] pcplus4D);
 
     always_ff @(posedge clk) begin
-        if (clr) begin
+        if (clr | reset) begin
             instrD <= 32'h00000000;
             pcD <= 32'h00000000;
             pcplus4D <= 32'h00000000;
         end 
-        else if (!enn) begin
+        else if (~enn) begin
             instrD <= instrF;
             pcD <= pcF;
             pcplus4D <= pcplus4F;
@@ -38,7 +38,7 @@ endmodule
 
 
 module ID_EX_datapipe(
-    input logic clk, clr,
+    input logic clk, clr, reset,
     input logic [31:0] rd1D, rd2D, instrD, pcD,
     output logic [31:0] rd1E, rd2E, instrE, pcE,
     input logic [4:0] rs1D, rs2D, rdD,
@@ -47,21 +47,21 @@ module ID_EX_datapipe(
     output [31:0] immextE, pcplus4E);
 
     always_ff @(posedge clk) begin
-        rd1E <= clr ? 32'h0 : rd1D;
-        rd2E <= clr ? 32'h0 : rd2D;
-        instrE <= clr ? 32'h0 : instrD;
-        pcE <= clr ? 32'h0 : pcD;
-        rs1E <= clr ? 5'h0 : rs1D;
-        rs2E <= clr ? 5'h0 : rs2D;
-        rdE <= clr ? 5'h0 : rdD;
-        immextE <= clr ? 32'h0 : immextD;
-        pcplus4E <= clr ? 32'h0 : pcplus4D;
+        rd1E <= (clr | reset) ? 32'h0 : rd1D;
+        rd2E <= (clr | reset) ? 32'h0 : rd2D;
+        instrE <= (clr | reset) ? 32'h0 : instrD;
+        pcE <= (clr | reset) ? 32'h0 : pcD;
+        rs1E <= (clr | reset) ? 5'h0 : rs1D;
+        rs2E <= (clr | reset) ? 5'h0 : rs2D;
+        rdE <= (clr | reset) ? 5'h0 : rdD;
+        immextE <= (clr | reset) ? 32'h0 : immextD;
+        pcplus4E <= (clr | reset) ? 32'h0 : pcplus4D;
     end
 endmodule
 
 
 module EX_MEM_datapipe(
-    input logic clk,
+    input logic clk, reset,
     input logic [31:0] aluresultE, writedataE, pcE,
     output logic [31:0] aluresultM, writedataM, pcM,
     input logic [4:0] rdE,
@@ -70,19 +70,19 @@ module EX_MEM_datapipe(
     output logic [31:0] instrM, pcplus4M);
 
     always_ff @(posedge clk) begin
-        aluresultM <= aluresultE;
-        writedataM <= writedataE;
-        pcM <= pcE;
-        instrM <= instrE;
-        pcplus4M <= pcplus4E;
-        rdM <= rdE;
+        aluresultM <= reset ? 32'h0 : aluresultE;
+        writedataM <= reset ? 32'h0 : writedataE;
+        pcM <= reset ? 32'h0 : pcE;
+        instrM <= reset ? 32'h0 : instrE;
+        pcplus4M <= reset ? 32'h0 : pcplus4E;
+        rdM <= reset ? 5'h0  : rdE;
     end
 
 endmodule
 
 
 module MEM_WB_datapipe(
-    input logic clk, 
+    input logic clk, reset,
     input logic [31:0] aluresultM, readdataM,
     output logic [31:0] aluresultW, readdataW,
     input logic [31:0] pcM, instrM, pcplus4M,
@@ -91,12 +91,11 @@ module MEM_WB_datapipe(
     output logic [4:0] rdW);
 
     always_ff @(posedge clk) begin
-        aluresultW <= aluresultM;
-        readdataW <= readdataM;
-        pcW <= pcM;
-        instrW <= instrM;
-        pcplus4W <= pcplus4M;
-        rdW <= rdM;
+        aluresultW <= reset ? 32'h0 : aluresultM;
+        readdataW <= reset ? 32'h0 : readdataM;
+        pcW <= reset ? 32'h0 : pcM;
+        instrW <= reset ? 32'h0 : instrM;
+        pcplus4W <= reset ? 32'h0 : pcplus4M;
+        rdW <= reset ? 5'h0 : rdM;
     end
-
 endmodule
