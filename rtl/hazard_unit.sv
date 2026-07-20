ac2aa624c65034c7f4d919bb278f591fb5f70ffe
vedsavjani <veddsavjani@gmail.com>
2026-07-20 12:04:39 +0530

    hazard unit manages caches stalls now

--- rtl/hazard_unit.sv
+++ rtl/hazard_unit.sv
@@ -1,11 +1,13 @@
 module hazard_unit(
     input logic [4:0] rs1D, rs2D, rs1E, rs2E, rdE, rdM, rdW,
     input logic pcsrcE, ResultSrcE0, RegWriteM, RegWriteW, RegWriteE,
-    output logic stallF, stallD, flushD, flushE,
+    input logic icache_hit, dcache_hit,
+    output logic stallF, stallD, stallE, stallM, 
+    output logic flushD, flushE,flushW,
     output logic [1:0] forwardAE, forwardBE,
     input logic [2:0] ResultSrcM, ResultSrcE);
 
-    logic lwstall, memstall, exstall;
+    logic lwstall, memstall, exstall, icache_stall, dcache_stall;
 
     always_comb begin
         if ((rs1E !=0) & (RegWriteM & (rs1E == rdM))) forwardAE = 2'b10;
@@ -20,15 +22,23 @@ module hazard_unit(
     end
 
     always_comb begin
+        icache_stall = ~icache_hit;
+        dcache_stall = ~dcache_hit;
+
         lwstall = ResultSrcE0 & ((rs1D == rdE) | (rs2D == rdE));
+
         memstall = (ResultSrcM != 3'b000) & RegWriteM & ((rs1E == rdM & rs1E != 5'b0) | (rs2E == rdM & rs2E != 5'b0));
         exstall = (ResultSrcE != 3'b000) & RegWriteE & ((rs1D == rdE & rs1D != 5'b0) | (rs2D == rdE & rs2D != 5'b0));
-        stallF = lwstall | memstall | exstall;
-        stallD = lwstall | memstall | exstall;
+
+        stallF = lwstall | memstall | exstall | icache_stall | dcache_stall;
+        stallD = lwstall | memstall | exstall | icache_stall | dcache_stall;
+        stallE = dcache_stall;
+        stallM = dcache_stall;
     end
 
     always_comb begin
-        flushD = pcsrcE;
-        flushE = lwstall | pcsrcE | memstall | exstall;
+        flushD = pcsrcE & ~(icache_stall | dcache_stall);
+        flushE = (lwstall | pcsrcE | memstall | exstall | icache_stall) & ~dcache_stall;
+        flushW = dcache_stall;
     end
 endmodule
